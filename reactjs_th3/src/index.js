3c432d8d46d09fce3e23761165c21f3cdb02c9f6
hanh361 <hanhltmt3@gmail.com>
2022-05-04 10:25:32 +0700

    nop bai tap

--- reactjs_th3/src/index.js
+++ reactjs_th3/src/index.js
@@ -8,7 +8,8 @@ import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
 import 'bootstrap/dist/css/bootstrap.min.css';
 const root = createRoot(container);
-root.render(<ListPage tab=" List " />);
+//root.render(<ListPage tab=" List " />);
+root.render(<HomePage tab="list" />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
